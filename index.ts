40666b12a4b65f6925ed98cf694f28317113c865
unknown <clerc.steven@gmail.com>
2023-12-23 15:26:59 +0100

    init project for my boiz

---
+++ index.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { Parser } from '@json2csv/plainjs';
+import * as fs from 'fs';
+
+const setAbbreviation = 'CLB';
+
+const requestObject: AxiosRequestConfig = {
+  method: 'get',
+  url:  `https://api.scryfall.com/cards/search?order=cmc&q=c%3Ared+pow%3D3`, //`https://api.scryfall.com/sets/${setAbbreviation}`,
+  responseType: 'json',
+}
+
+axios(requestObject).then((jsonResult: AxiosResponse) => {
+  try {
+    console.log(jsonResult.data.data);
+    const sets = jsonResult.data.data.map((set: any) => ({'number': set.collector_number, 'code': set.code, 'name': set.name, 'set': set.set}));
+    const fields = ['number', 'code', 'name', 'set'];
+    const opts = {fields};
+    const parser = new Parser(opts);
+
+    const csv = parser.parse(sets);
+
+    fs.writeFile('./csv-files/test.csv', csv, (err: any) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  } catch(e) {
+    console.error(e);
+  }
+})
