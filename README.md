4229ddd20cb2ad6abb366b2adedc8e35575edd76
unknown <clerc.steven@gmail.com>
2023-12-23 17:14:13 +0100

    add some type declaration

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # mtg-csv
+
+Be sure that nodeJs is installed on your pc
+
+1. retrieve the project
+2. open a terminal from the project folder
+3. npm install
+4. run `npx ts-node index.ts` 
+5. your file is inside the folder `./csv-files`
+
+if you want more, you can update the script.
--- index.ts
+++ index.ts
@@ -2,18 +2,19 @@ import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { Parser } from '@json2csv/plainjs';
 import * as fs from 'fs';
 
+const baseUrl = 'https://api.scryfall.com';
 const setAbbreviation = 'CLB';
 
 // TODO: make the url adaptable
 const requestObject: AxiosRequestConfig = {
   method: 'get',
-  url:  `https://api.scryfall.com/cards/search?order=cmc&q=c%3Ared+pow%3D3`, //`https://api.scryfall.com/sets/${setAbbreviation}`,
+  url:  `${baseUrl}/cards/search?order=cmc&q=c%3Ared+pow%3D3`, //`https://api.scryfall.com/sets/${setAbbreviation}`,
   responseType: 'json',
 }
 
-axios(requestObject).then((jsonResult: AxiosResponse) => {
+  axios(requestObject).then((jsonResult: AxiosResponse<SearchResult>) => {
   try {
-    console.log(jsonResult.data.data);
+    console.log(jsonResult.data.data[0]);
     const sets = jsonResult.data.data.map((set: any) => ({'number': set.collector_number, 'code': set.code, 'name': set.name, 'set': set.set}));
     const fields = ['number', 'code', 'name', 'set'];
     const opts = {fields};
