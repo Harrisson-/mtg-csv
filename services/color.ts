434d00f95f05300b95f71495e76c51ec32698d52
unknown <clerc.steven@gmail.com>
2024-07-02 23:41:15 +0200

    add rgbtohex function and builder function

--- services/color.ts
+++ services/color.ts
@@ -2,15 +2,27 @@
 /**
  * Based on https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb/5624139#5624139
  * 
- * @param colorHex {string}
+ * @param hexColor {string}
  * @returns 
  */
-function hextoRGB(colorHex: string): number[] {
-  const colorArray = colorHex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, (_m, r, g, b) => '#' + r + r + g + g + b + b)
+function hextoRGB(hexColor: string): number[] {
+  const colorArray = hexColor.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, (_m, r, g, b) => '#' + r + r + g + g + b + b)
   .substring(1).match(/.{2}/g) ?? [];
-  return colorArray.map(x => parseInt(x, 16));
+  return colorArray.map(color => parseInt(color, 16));
 }
 
+/**
+ * Based on https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb/5624139#5624139
+ * 
+ * @param rgbColor {number[]}
+ * @returns { string }
+ */
+function rgbToHex(rgbColor: number[]): string {
+  const r = rgbColor[0].toString(16);
+  const g = rgbColor[1].toString(16);
+  const b = rgbColor[2].toString(16);
+  return "#" + (r.length == 1 ? "0" + r : r) + (g.length == 1 ? "0" + g : g) + (b.length == 1 ? "0" + b : b);
+}
 
 /**
  * 
@@ -36,6 +48,8 @@ function createPalette(colorsArray: number[][], stepBetween: number): number[][]
   return palette;
 }
 
-function rgbToHex(colorArray: number[]) {
-
+export function buildPalette(colorsArray: string[], stepBetween: number): string[] {
+  const rgbcolors = colorsArray.map(color => hextoRGB(color));
+  const palette = createPalette(rgbcolors, stepBetween);
+  return palette.map(rgbcolor => rgbToHex(rgbcolor));
 }
